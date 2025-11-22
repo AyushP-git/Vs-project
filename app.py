e74514c5345556c820cc0eb0e259fd280cf3b2ac
Ayush Patel <ayushpatel31@usf.edu>
2025-11-22 15:37:18 -0500

    Add formatted output displaying calculation results

--- app.py
+++ app.py
@@ -2,3 +2,4 @@ print("Welcome to my Python program!")
 miles = input("How many miles did you walk today")
 miles = float(miles)
 weekly_miles = miles * 7
+print(f"You are on track to walk {weekly_miles} miles this week.")

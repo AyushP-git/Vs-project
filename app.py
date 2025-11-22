7340eebdb68d9c666d9846d0689c8cb73a70c2b1
Ayush Patel <ayushpatel31@usf.edu>
2025-11-22 15:51:14 -0500

    Add basic error handling for user input

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 print("Welcome to my Python program!")
 miles = input("How many miles did you walk today")
-miles = float(miles)
+
+try: 
+    miles = float(miles)
+except ValueError:
+    print("please enter a valid number")
+    exit()
 weekly_miles = miles * 7
 print(f"You are on track to walk {weekly_miles} miles this week.")
